dc570f7dcd210d9a84e1c501e175202db227f293
Niko Kodzhabashev <nkodzhabashev@centroida.ai>
2018-04-10 16:25:14 +0300

    fixed conflicts

--- server/models/store.js
+++ server/models/store.js
@@ -3,18 +3,6 @@ const mongoose = require('mongoose');
 
 // store Schema and Model
 
-<<<<<<< HEAD
-const StoreSchema = new Schema({
-    Name: {
-        type: String,
-        minlength: 1,
-        maxlength: 100,
-        required:[true,'Name field is required']
-    }
-});
-
-const Store = mongoose.model('store', StoreSchema);
-=======
 const StoreSchema = new mongoose.Schema({
     Name: {
         type: String,
@@ -26,5 +14,4 @@ const StoreSchema = new mongoose.Schema({
 });
 
 const Store = mongoose.model('author', StoreSchema);
->>>>>>> cef8f9ab24dc90195eab7d0a50e91dddf8cbee6c
 module.exports = Store;
